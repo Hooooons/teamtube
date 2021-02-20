4ee38e90c0f788a4962ea8cbbdaf7ee116faa33e
Hooooons <59018625+Hooooons@users.noreply.github.com>
2021-02-20 14:01:52 +0900

    리덕스 기본 프레임 구성
    리덕스 기본 프레임 구성

--- src/index.js
+++ src/index.js
@@ -5,13 +5,33 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+import ReduxSaga from "redux-saga";
+
+import rootReducer from "./redux";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const createStoreWithMiddleware = applyMiddleware(
+  ReduxThunk,
+  ReduxSaga
+)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider
+      store={createStoreWithMiddleware(
+        rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
