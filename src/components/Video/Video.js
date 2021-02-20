374aa7b4b642a41d47089de531fd7cbcfd8f10c2
jk9804034 <lovelykjt@gmail.com>
2021-02-20 15:57:46 +0900

    비디오 추가

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 function Video() {
-  return <div>Video</div>;
+  return <div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://i.ytimg.com/vi/mkgg9LC9Ibw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDXc4IbNCSPRJ9YTNT6oTBSQpc6jQ" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  </div>;
 }
 
 export default Video;
